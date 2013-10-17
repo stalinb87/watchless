ced39b1bf73731260f6bc26cb6cef5de0a72e64c
Stalin Bonilla <sbonilla@dominet.net>
2013-10-17 11:57:19 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+var watch = require('watch');
+var argv = require('optimist').argv;
+var fs = require('fs');
+var less = require('less');
+var path = require('path');
+
+var output = argv.o;
+var file = argv.f;
+var folder = argv.d;
+
+//
+if(argv.h) {
+  console.log('Supposed to show help');
+  process.exit();
+} 
+console.log('Waiting for changes in folder '+folder);
+var compile = function(f, stat, prev) {
+  fs.readFile(file, function(err, content){
+    if(err){
+      console.log("an error happend while trying to read the file "+file);
+      console.log(err);
+      process.exit();
+    } else {      
+      var parser = new (less.Parser)({
+        paths:[path.dirname(file)],
+        filename:path.basename(file)
+      });
+      parser.parse(content.toString(), function (err, tree){
+        if(err) {
+          console.log("an error happend while trying to compile the less file");
+          console.log(err);
+        }  else {                  
+          fileToWrite = path.basename(file, '.less');
+          fileToWrite+= ".css";
+          outputFile = path.join(output, fileToWrite);
+          var css = tree.toCSS({compress:true});
+          fs.writeFile(outputFile, css, function (err){
+            if(err) {
+              trow(err);
+              console.log("an error happend while trying to save the file to the output "+ output);
+              console.log(err);
+            } else {
+              console.log('compiled file '+file +' on '+outputFile);
+            }
+          });
+        }
+      });
+    }
+  });
+}
+compile();
+watch.createMonitor(argv.d, function(monitor){
+  monitor.on("created", compile);
+  monitor.on("changed", compile);
+  monitor.on("removed", compile);
+}); 
